fa2dff397c0a4b333b36eec5c1add2dc072f1e38
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-01-29 07:21:11 +0200

    fix

--- Regex/regex.js
+++ Regex/regex.js
@@ -13,13 +13,11 @@ function capitalA(s) {
 /* Write a function that take a string and return true if the the sting is following the emails pattern
 which end with io (example@example.io) */
 
-let arr = [];
-function imagesSearcher(text) {
-  let regex = /\w*.png\b|\w*.jpeg\b|\w*.jpg\b/g;
-  arr = text.match(regex);
-  if ((arr = null)) {
-    return [];
-  } else return arr;
+function ioEmail(email) {
+  let result;
+  let regex = /^[a-zA-Z0-9]+@example+.io$/;
+  result = regex.test(email);
+  return result;
 }
 
 /* You have a text that contain image names with their extention you need to write a function to 
@@ -30,5 +28,7 @@ let arr = [];
 function imagesSearcher(text) {
   let regex = /\w*.png\b|\w*.jpeg\b|\w*.jpg\b/g;
   arr = text.match(regex);
-  return arr;
+  if (arr == null) {
+    return [];
+  } else return arr;
 }
--- Regex/regex.test.js
+++ Regex/regex.test.js
@@ -27,7 +27,9 @@ let arr = [];
 function imagesSearcher(text) {
   let regex = /\w*.png\b|\w*.jpeg\b|\w*.jpg\b/g;
   arr = text.match(regex);
-  return arr;
+  if (arr == null) {
+    return [];
+  } else return arr;
 }
 
 describe("Test capitalA", () => {
@@ -60,11 +62,11 @@ describe("Test imagesSearcher", () => {
       imagesSearcher(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       )
-    ).toStrictEqual(null);
+    ).toStrictEqual([]);
     expect(
       imagesSearcher(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. horse.gif Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore dolphin.pdf eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa mouse.tiff qui officia deserunt mollit anim id est laborum."
       )
-    ).toStrictEqual(null);
+    ).toStrictEqual([]);
   });
 });

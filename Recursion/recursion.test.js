8018073a5d42efad6615af007e840c6f830b0d5e
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-01-29 05:12:12 +0200

    fix

--- Recursion/recursion.test.js
+++ Recursion/recursion.test.js
@@ -1,4 +1,50 @@
-"use strict";
+/* Write a function to do the division operation without using the built-in division*/
+
+function division(number, dividedBy) {
+  // Write you logic here.
+  return;
+}
+
+/* Write a function that implement Math.pow(x,n) but using recursion
+Example:
+pow(2,4) = 16
+*/
+
+function pow(x, n) {
+  // Write you logic here.
+  return;
+}
+
+/* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
+the next integer is determined by summing the previous two. This gives us:
+0, 1, 1, 2, 3, 5, 8, 13, ...
+Write a function that take n as parameter and return the nth element in the Fibonacci Series
+Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
+
+function fibonacci(n) {
+  // Write you logic here.
+  return;
+}
+
+/* Optional 
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+Example: 
+Input: n = 3, k = 3
+Output: ["123", "132", "213", "231", "312", "321"] */
+
+function permutations(n, k) {
+  let arr = [];
+  // Write you logic here.
+  return arr;
+}
 
 describe("Test division", () => {
   test("Return the division result", () => {
--- Regex/regex.test.js
+++ Regex/regex.test.js
@@ -1,5 +1,32 @@
 "use strict";
 
+/* Write a function that take a string and return true if the string only contain uppercase and lowercase
+characters (no numbers and symbols) and it should end with capital A else return false */
+
+function capitalA(s) {
+  // Add your logic.
+  return;
+}
+
+/* Write a function that take a string and return true if the the sting is following the emails pattern
+which end with io (example@example.io) */
+
+function ioEmail(email) {
+  // Add your logic.
+  return;
+}
+
+/* You have a text that contain image names with their extention you need to write a function to 
+find all images in that text and return their names and extention in an array 
+required extention are jpg, jpeg and png.
+*/
+
+function imagesSearcher(text) {
+  let arr = [];
+  // Add your logic.
+  return arr;
+}
+
 describe("Test capitalA", () => {
   test("It should return true if the input has uppercase and lowercase characters (no numbers and symbols) and it should end with capital A else return false ", () => {
     expect(capitalA("Hello world A")).toStrictEqual(true);

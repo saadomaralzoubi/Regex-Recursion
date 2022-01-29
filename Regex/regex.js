7b3e0c0b395828b4422a77683afc3cc2e80f0f92
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-01-29 07:16:33 +0200

    fix

--- Regex/regex.js
+++ Regex/regex.js
@@ -13,11 +13,13 @@ function capitalA(s) {
 /* Write a function that take a string and return true if the the sting is following the emails pattern
 which end with io (example@example.io) */
 
-function ioEmail(email) {
-  let result;
-  let regex = /^[a-zA-Z0-9]+@example+.io$/;
-  result = regex.test(email);
-  return result;
+let arr = [];
+function imagesSearcher(text) {
+  let regex = /\w*.png\b|\w*.jpeg\b|\w*.jpg\b/g;
+  arr = text.match(regex);
+  if ((arr = null)) {
+    return [];
+  } else return arr;
 }
 
 /* You have a text that contain image names with their extention you need to write a function to 
--- Regex/regex.test.js
+++ Regex/regex.test.js
@@ -60,11 +60,11 @@ describe("Test imagesSearcher", () => {
       imagesSearcher(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       )
-    ).toStrictEqual([]);
+    ).toStrictEqual(null);
     expect(
       imagesSearcher(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. horse.gif Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore dolphin.pdf eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa mouse.tiff qui officia deserunt mollit anim id est laborum."
       )
-    ).toStrictEqual([]);
+    ).toStrictEqual(null);
   });
 });
